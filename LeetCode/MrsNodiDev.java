010a0f40bc8d9b844fa7444359cc56ec03874e63
Pranto Dev <prantodev@Prantos-MacBook-Air.local>
2022-09-25 14:03:14 +0600

    Initial commit

---
+++ LeetCode/MrsNodiDev.java
@@ -0,0 +1,9 @@
+package LeetCode;
+
+public class MrsNodiDev {
+    public static void main(String[] args) {
+        for (int i=0; i<10; i++){
+            System.out.println("I love You Mrs. Nodi Dev <3 ");
+        }
+    }
+}
---
+++ hello.java
@@ -0,0 +1,8 @@
+public class hello{
+    public static void main(String[] args) {
+        System.out.println("hello");
+        for (int index = 0; index < 100; index++) {
+            System.out.println("I Love You Nodi, my dear wife ");
+        }
+    }
+}
